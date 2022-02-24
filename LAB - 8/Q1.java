de883f47c569b0da7356059610c8f81b37adfc38
Anupam <anupam205@outlook.com>
2022-02-24 09:28:17 +0530

    edit

---
+++ LAB - 8/Q1.java
@@ -0,0 +1,10 @@
+/* 1. Write a program in java to implement a class Employee having data members name, employee_id, salary . 
+Include default, parameterized and copy constructor to initialize data members of the class and also copy an employee information. 
+A member function show() which is used to display all information about an employee. Use InputStreamReader and BufferedReader 
+classes to take input values for the employee.*/
+
+public class Q1 {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
---
+++ LAB - 8/Q2.java
@@ -0,0 +1,76 @@
+/*2. Write a program to find area of Circle, Triangle and Rectangle using constructor overloading concept.*/
+
+import java.util.*;
+
+class Area {
+    private int radius, length, breadth, arRectangle;
+    private final float pi = 3.14f, half = 0.5f;
+    private float base, height, arCircle, arTriangle;
+
+    Area() {
+        radius = 0;
+        length = 0;
+        breadth = 0;
+        base = 0.0f;
+        height = 0.0f;
+    }
+
+    void input(int r) {
+        radius = r;
+    }
+
+    void input(int l, int b) {
+        length = l;
+        breadth = b;
+    }
+
+    void input(float b, float h) {
+        base = b;
+        height = h;
+    }
+
+    void AreaCircle() {
+        arCircle = pi * radius * radius;
+        System.out.println("Area of Circle is " + arCircle + "cm.square");
+    }
+
+    void AreaTriangle() {
+        arTriangle = half * base * height;
+        System.out.println("Area of Triangle is " + arTriangle + "cm.square");
+    }
+
+    void AreaRectangle() {
+        arRectangle = length * breadth;
+        System.out.println("Area of Rectangle is " + arRectangle + "cm.square");
+    }
+}
+
+class Q2 {
+    public static void main(String arg[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Radius of Circle as integer");
+        int r = sc.nextInt();
+
+        System.out.println("Enter the Length and Breadth of Rectangle as integer");
+        int len = sc.nextInt();
+        int bre = sc.nextInt();
+
+        System.out.println("Enter the Base and height of Triangle as float");
+        float b = sc.nextFloat();
+        float h = sc.nextFloat();
+
+        Area circle = new Area();
+        Area rectangle = new Area();
+        Area triangle = new Area();
+
+        circle.input(r);
+        rectangle.input(len, bre);
+        triangle.input(b, h);
+
+        circle.AreaCircle();
+        rectangle.AreaRectangle();
+        triangle.AreaTriangle();
+        sc.close();
+    }
+}
